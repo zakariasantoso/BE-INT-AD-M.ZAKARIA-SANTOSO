988847900cda647c8daf344c4d153d9bd2e13af6
zakaria santoso <santosozakaria29@gmail.com>
2024-09-18 23:51:38 +0700

    Refactor respondWithToken method to return a JSON response with additional data

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -51,10 +51,20 @@ class AuthController extends Controller
      */
     protected function respondWithToken($token)
     {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
-        ]);
+        // return response()->json([
+        //     'access_token' => $token,
+        //     'token_type' => 'bearer',
+        //     'expires_in' => auth()->factory()->getTTL() * 60
+        // ]);
+        return response()->json(
+            [
+                'status' => 'success',
+                'data' => [
+                    'access_token' => $token,
+                    'token_type' => 'bearer',
+                    'expires_in' => auth()->factory()->getTTL() * 60
+                ]
+            ]
+        );
     }
 }
